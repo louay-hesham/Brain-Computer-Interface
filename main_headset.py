896c8bb3b78365519e0ace300a07b006521e19f4
Louay Morsi <louay.hesham@gmail.com>
2019-04-20 13:21:52 +0200

    Added model, and modified code to work with the model

--- main_headset.py
+++ main_headset.py
@@ -1,12 +1,15 @@
 import time
 from emokit.headset import Headset
+import pickle
+import xgboost as xgb
 
 if __name__ == "__main__":
     with Headset() as headset:
+        model = pickle.load(open("model.dat", "rb"))
         while True:
             sample = headset.get_sample(print=True)
             if sample is not None:
-                # print(data)
-                sample
-                # Do something with raw data here
+                sample = xgb.DMatrix(sample)
+                prediction = model.predict(sample)
+                print("Prediction: " + prediction)
             time.sleep(0.05)
