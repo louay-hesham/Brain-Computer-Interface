7aa14aeb377d8b6277a82b65245f6f225a012315
Louay Morsi <louay.hesham@gmail.com>
2019-05-03 22:11:56 +0200

    Small changes

--- emotiv_test.py
+++ emotiv_test.py
@@ -2,9 +2,9 @@ import time
 from emokit.headset import Headset
 
 if __name__ == "__main__":
-    with Headset() as headset:
+    with Headset(write=True, force_old_crypto=True) as headset:
         while True:
-            data = headset.get_sensors_raw_data(print=True)
+            data = headset.get_sensors_raw_data(print_output=True)
             if data is not None:
                 data
                 # Do something with raw data here
