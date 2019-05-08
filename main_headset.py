56bd5ddec62e5774fab93fa7b246ef9c3b83158c
Louay Morsi <louay.hesham@gmail.com>
2019-05-08 18:07:14 +0200

    Added delay and increased sample count

--- main_headset.py
+++ main_headset.py
@@ -8,7 +8,9 @@ from prediction import extract_features
 if __name__ == "__main__":
     model = pickle.load(open("model.dat", "rb"))
     with Headset() as headset:
-        samples = headset.get_samples_fft(10, 1/128, print_output=False)
+        print("Quering in 5 seconds")
+        time.sleep(5)
+        samples = headset.get_samples_fft(100, 1/128, print_output=False)
     samples_extra_features, label_testing = extract_features(samples, print_log=True)
     samples_extra_features = np.array(samples_extra_features)
     samples_extra_features = xgb.DMatrix(samples_extra_features)
