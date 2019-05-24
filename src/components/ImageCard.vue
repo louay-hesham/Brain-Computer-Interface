5687d4b1818eb1269ec4dec6bc764eb6c1785514
yomnabarake21 <yomnabarakate21@gmail.com>
2019-05-24 18:25:24 +0200

    Add ImagCard component

---
+++ src/components/ImageCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="image">
+    <img src="../assets/standing.jpg" v-if="prediction == -1">
+    <img src="../assets/RightHand.jpg" v-if="prediction == 2">
+    <img src="../assets/LeftHand.jpg" v-if="prediction == 3">
+    <img src="../assets/EyesClosed2.jpg" v-if="prediction == 4">
+    <img src="../assets/BothFeets.jpg" v-if="prediction== 5">
+  </div>
+</template>
+<script>
+export default {
+  name: 'imageCard',
+  props: ['prediction']
+}
+</script>
+
+<style scoped>
+.image{
+  width:150px;
+  height:auto;
+  margin-left:30%;
+  margin-right:30%;
+  margin-top:2%;
+  margin-bottom:2%;
+}
+</style>
+
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import SuiVue from 'semantic-ui-vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 Vue.use(SuiVue);
+Vue.use(VueAxios, axios)
+Vue.config.productionTip = false
 new Vue({
   el: '#app',
   render: h => h(App)
