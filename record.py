e5753b16f94a50dc45fa95a2c27575fbd357bc0b
Louay Morsi <louay.hesham@gmail.com>
2019-06-25 13:03:06 +0200

    Fixed prediction.py

--- record.py
+++ record.py
@@ -2,7 +2,12 @@ import time
 from emokit.headset import Headset
 
 if __name__ == "__main__":
+    total_data = []
+    time.sleep(5)
+    timeout = time.time() + 30
     with Headset(write=True) as headset:
-        while True:
+        while time.time() < timeout:
             data = headset.get_sample(print_output=True)
+            total_data.append(data)
             time.sleep(1/128)
+        print(total_data)
