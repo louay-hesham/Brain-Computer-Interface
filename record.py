968955e1ad59f0a76df6ff9cbad1c73de5593418
Louay Morsi <louay.hesham@gmail.com>
2019-06-25 12:12:32 +0200

    Added record python script

--- record.py
+++ record.py
@@ -2,6 +2,14 @@ import time
 from emokit.headset import Headset
 
 if __name__ == "__main__":
+    print("Starting in 10 seconds")
+    time.sleep(7)
+    print("Starting in 3 seconds")
+    time.sleep(1)
+    print("Starting in 2 seconds")
+    time.sleep(1)
+    print("Starting in 1 seconds")
+    time.sleep(1)
     with Headset(write=True) as headset:
         while True:
             data = headset.get_sample(print_output=True)
